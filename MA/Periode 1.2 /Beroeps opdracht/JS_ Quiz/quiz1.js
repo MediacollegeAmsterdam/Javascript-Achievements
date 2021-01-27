a3d77a8eaaac53486d4a9fa55633484d42143f54
Robin schoenmaker <macbookprorobin@MacBook-Pro-van-Robin.local>
2021-01-27 15:07:31 +0100

    cool

---
+++ MA/Periode 1.2 /Beroeps opdracht/JS_ Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "StreatsDesign Quiz",
+    "quizMetaData":
+    {
+        "title": "LStreatsDesign Quiz",
+        "imageURI": "https://streatsdesign.com/StreatsDesign_Logo.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Waar vind je Streatsdesign?",
+				"answers": [{
+						"answer": "Online?",
+						"feedback": true
+					},
+					{
+						"answer": "In Langedijk",
+						"feedback": true
+					},
+					{
+						"answer": "In Amsterdam",
+						"feedback": false
+					},
+					{
+						"answer": "Zuid Holland",
+						"feedback": false
+					},
+					{
+						"answer": "Alleen op een vaste locatie",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Waar is StreatsDesign op gebaseerd?",
+				"answers": [{
+						"answer": "Street Design",
+						"feedback": false
+					},
+					{
+						"answer": "Design, Dtp en meer",
+						"feedback": true
+					},
+					{
+						"answer": "Teken en grafish werk",
+						"feedback": false
+					},
+					{
+						"answer": "Handmade products",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wie is de Eigenaar van Steatsdesig: ?",
+				"answers": [{
+						"answer": "Robin Schoenmaker",
+						"feedback": true
+					},
+					{
+						"answer": "Ronald Spruit",
+						"feedback": false
+					},
+					{
+						"answer": "Amber Zijp",
+						"feedback": false
+					},
+					{
+						"answer": "Kaaskop",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
